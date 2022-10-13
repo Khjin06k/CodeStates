4651e2b027211a57d43549fff6f45017e5876f99
김희진 <khj4209kk@gmail.com>
2022-10-13 18:24:03 +0900

    싱글톤 실습 진행

--- Section2/section2-week4/src/main/java/com/codestates/section2week4/DependencyConfig.java
+++ Section2/section2-week4/src/main/java/com/codestates/section2week4/DependencyConfig.java
@@ -2,18 +2,23 @@ package com.codestates.section2week4;
 
 import com.codestates.section2week4.Member.*;
 import com.codestates.section2week4.Coffee.*;
+import org.springframework.context.annotation.*;
 
+@Configuration
 public class DependencyConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberRepository();
     }
-
+    @Bean
     public CoffeeService coffeeService(){
         return new CoffeeService(coffeeRepository());
     }
+    @Bean
     public CoffeeRepository coffeeRepository(){
         return new CoffeeRepository();
     }
--- Section2/section2-week4/src/main/java/com/codestates/section2week4/SingletonTest.java
+++ Section2/section2-week4/src/main/java/com/codestates/section2week4/SingletonTest.java
@@ -2,7 +2,7 @@ package com.codestates.section2week4;
 
 import com.codestates.section2week4.Member.*;
 import com.codestates.section2week4.Singleton.SingletonService;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
     /*
@@ -17,7 +17,8 @@ public class SingletonTest {
     }
     객체의 주소값이 다름 >> 수많은 객체를 사용할 경우 그만큼 메모리 낭비와 효율성이 떨어지기 때문에 싱글톤 패턴을 사용해 해결이 가능함
     */
-    static SingletonService singletonService1 = SingletonService.getInstance();
+
+    /*static SingletonService singletonService1 = SingletonService.getInstance();
     static SingletonService singletonService2 = SingletonService.getInstance();
 
     public static void main(String[] args) {
@@ -26,5 +27,14 @@ public class SingletonTest {
     }
     //주소값이 동일함 (하나의 인스턴스를 공유하기 때문에 주소값이 동일)
     // >> 모든 객체가 같은 인스턴스를 바라보게 되며 메모리 낭비가 덜하고 효율성이 올라감
+    */
 
+    static AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(DependencyConfig.class);
+    static MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+    static MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+    public static void main(String[] args) {
+        System.out.println("memberService1 : " + memberService1);
+        System.out.println("memberService2 : " + memberService2);
+    }
 }
