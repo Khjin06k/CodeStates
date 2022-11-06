25343adbede886a2a1339320d221baffd410a503
김희진 <khj4209kk@gmail.com>
2022-11-06 15:24:14 +0900

    Stamp 구현

--- Section3/be-homework-jpa-main/src/main/java/com/codestates/member/entity/Stamp.java
+++ Section3/be-homework-jpa-main/src/main/java/com/codestates/member/entity/Stamp.java
@@ -1,4 +1,35 @@
 package com.codestates.member.entity;
 
-public class Stamp {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Stamp { //Member와 일대일 매핑
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long stampId;
+
+    private int stampCount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+    public void addMember(Member member) {
+        this.member = member;
+    }
+
 }
