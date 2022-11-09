e3dd18be1d926f8c1ebfdb417f280b3767b0093a
김희진 <khj4209kk@gmail.com>
2022-11-09 11:28:54 +0900

    init

--- Section3/be-template-testing-main/src/main/java/com/codestates/member/dto/MemberDto.java
+++ Section3/be-template-testing-main/src/main/java/com/codestates/member/dto/MemberDto.java
@@ -5,6 +5,7 @@ import com.codestates.stamp.Stamp;
 import com.codestates.validator.NotSpace;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -12,6 +13,7 @@ import javax.validation.constraints.Pattern;
 
 public class MemberDto {
     @Getter
+    @Setter
     public static class Post {
         @NotBlank
         @Email
@@ -23,9 +25,11 @@ public class MemberDto {
         @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$",
                 message = "휴대폰 번호는 010으로 시작하는 11자리 숫자와 '-'로 구성되어야 합니다.")
         private String phone;
+
     }
 
     @Getter
+    @Setter
     public static class Patch {
         private long memberId;
 
