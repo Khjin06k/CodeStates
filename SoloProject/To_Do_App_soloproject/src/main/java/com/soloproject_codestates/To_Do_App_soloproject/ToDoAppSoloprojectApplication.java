18351614d57ef04a53c0b95f2ca907b7ddd94de9
김희진 <khj4209kk@gmail.com>
2022-12-10 12:13:49 +0900

    init

---
+++ SoloProject/To_Do_App_soloproject/src/main/java/com/soloproject_codestates/To_Do_App_soloproject/ToDoAppSoloprojectApplication.java
@@ -0,0 +1,13 @@
+package com.soloproject_codestates.To_Do_App_soloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppSoloprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppSoloprojectApplication.class, args);
+	}
+
+}
---
+++ SoloProject/To_Do_App_soloproject/src/test/java/com/soloproject_codestates/To_Do_App_soloproject/ToDoAppSoloprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soloproject_codestates.To_Do_App_soloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppSoloprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
