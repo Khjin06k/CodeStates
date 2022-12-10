731257ce22851916295cdad97159f29958c70190
김희진 <khj4209kk@gmail.com>
2022-12-10 13:11:59 +0900

    Mapper 구현

---
+++ SoloProject/To_Do_App_soloproject/src/main/java/com/soloproject_codestates/To_Do_App_soloproject/mapper/TaskMapper.java
@@ -0,0 +1,14 @@
+package com.soloproject_codestates.To_Do_App_soloproject.mapper;
+
+import com.soloproject_codestates.To_Do_App_soloproject.dto.TasksPatchDto;
+import com.soloproject_codestates.To_Do_App_soloproject.dto.TasksPostDto;
+import com.soloproject_codestates.To_Do_App_soloproject.dto.TasksResponseDto;
+import org.mapstruct.Mapper;
+import org.springframework.scheduling.config.Task;
+
+@Mapper(componentModel = "spring")
+public interface TaskMapper {
+    Task tasksPostDtoToTask(TasksPostDto tasksPostDto);
+    Task taskPatchDtoToTask(TasksPatchDto tasksPatchDto);
+    TasksResponseDto taskToTaskResponseDto(Task task);
+}
