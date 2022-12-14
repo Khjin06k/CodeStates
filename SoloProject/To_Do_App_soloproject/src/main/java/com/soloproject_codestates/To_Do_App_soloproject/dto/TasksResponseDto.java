ec3e1ad22a9b3b3f26471c2ccad6c5e76c770e39
김희진 <khj4209kk@gmail.com>
2022-12-15 01:16:21 +0900

    GetTasks, Patch 수정 필요 (Response 오류 해결)

--- SoloProject/To_Do_App_soloproject/src/main/java/com/soloproject_codestates/To_Do_App_soloproject/dto/TasksResponseDto.java
+++ SoloProject/To_Do_App_soloproject/src/main/java/com/soloproject_codestates/To_Do_App_soloproject/dto/TasksResponseDto.java
@@ -1,8 +1,10 @@
 package com.soloproject_codestates.To_Do_App_soloproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class TasksResponseDto {
     private int taskId;
     private String title;
